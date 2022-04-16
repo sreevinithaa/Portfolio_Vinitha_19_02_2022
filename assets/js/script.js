1d45b4585cd7cea4cf704d79e6e11cad90a56a97
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-04-17 07:57:00 +0800

    Update styles which support responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 $(".project_body").mouseover(function(event){
-    $(event.target).next("img").css("opacity",".1");
+   
+   
     var panel=$(event.target).next(".panel");
   
     panel.fadeIn("slow");
